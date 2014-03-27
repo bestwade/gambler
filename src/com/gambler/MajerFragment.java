d549c17294b1d4b9ed634485a1135bb1754dfd66
bestwade <bestwade4@gmail.com>
2014-03-27 18:30:48 +0800

    yaaa

--- src/com/gambler/MajerFragment.java
+++ src/com/gambler/MajerFragment.java
@@ -2,18 +2,24 @@ package com.gambler;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class MajerFragment extends Fragment {
- 
+	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
  
         View rootView = inflater.inflate(R.layout.majer, container, false);
-         
+        fragment1 fv = new fragment1();
+        FragmentManager  fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.layoutToReplace, fv);
+        ft.commit();
         return rootView;
     }
 }
